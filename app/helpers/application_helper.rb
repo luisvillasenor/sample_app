7bf1b3c92355d92ce4b51418f766d6294994767e
Luis Villasenor <luis@iceberg9.com>
2016-08-14 14:52:22 -0500

    Agrega full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	# Las funciones definidas aqui, Rails las hace disponibles en toda la App
+	# sin la necesidad de usar el include
+	def full_title(page_title = '')
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"			
+		end		
+	end
 end
